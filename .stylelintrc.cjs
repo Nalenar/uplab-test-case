68ea804789a78cb9e0f059d47bc16aa076b1199c
Nikita <nichrom.isme@gmail.com>
2024-08-02 11:24:05 +0300

    Added stylelint

---
+++ .stylelintrc.cjs
@@ -0,0 +1,6 @@
+/** @type {import('stylelint').Config} */
+
+module.exports = {
+  extends: ["stylelint-config-standard-scss", "stylelint-config-clean-order", "stylelint-config-recommended-vue/scss"],
+  plugins: ["stylelint-scss"]
+};
