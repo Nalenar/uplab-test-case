394968e7c8686c1eaadfbb5eed9e56844acbea0d
Nikita <nichrom.isme@gmail.com>
2024-08-03 20:08:18 +0300

    Added new components into App

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
   <Navbar />
+  <Hero />
+  <Articles />
 </template>
 
 <script setup lang="ts">
 import Navbar from "@components/layout/Navbar.vue";
+import Hero from "@components/layout/Hero.vue";
+import Articles from "@components/layout/Articles.vue";
 </script>
-
