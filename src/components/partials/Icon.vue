3bfc89d677d7784319334d4d0dd99dcc2d96d6fd
Nikita <nichrom.isme@gmail.com>
2024-08-04 18:32:29 +0300

    Added component for svg icons

---
+++ src/components/partials/Icon.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { defineAsyncComponent } from "vue";
+const props = defineProps<{ name: string }>();
+const icon = defineAsyncComponent(() => import(`@icons/${props.name}.svg`));
+</script>
+
+<template>
+  <component :is="icon" />
+</template>
