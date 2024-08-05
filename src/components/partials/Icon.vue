7b90753cac3b827b8cfd53572321ed4e38db0f4e
Nikita <nichrom.isme@gmail.com>
2024-08-05 21:33:38 +0300

    Typed name prop

--- src/components/partials/Icon.vue
+++ src/components/partials/Icon.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">
-import { defineAsyncComponent } from "vue";
-const props = defineProps<{ name: string }>();
+import { defineAsyncComponent, type PropType } from "vue";
+
+type IconType = "heart" | "comments" | "docs";
+
+const props = defineProps({
+  name: { type: String as PropType<IconType>, required: true },
+});
+
 const icon = defineAsyncComponent(() => import(`@icons/${props.name}.svg`));
 </script>
 
