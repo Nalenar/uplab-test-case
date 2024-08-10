7157357f74622c91ea8e609dcbd30cccc1d1cdad
Nikita <nichrom.isme@gmail.com>
2024-08-10 22:32:14 +0300

    Moved test screen into views folder

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import Home from "@/views/Home.vue";
-import Test from "@/Test.vue";
+import Test from "@/views/Test.vue";
 
 const router = createRouter({
   history: createWebHistory(),
