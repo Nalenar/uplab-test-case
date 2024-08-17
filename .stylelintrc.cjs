7db8a54941c141487cc4af1a062ea2e743ea010a
Nikita <nichrom.isme@gmail.com>
2024-08-18 02:03:45 +0300

    Changed selector class pattern

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -2,5 +2,13 @@
 
 module.exports = {
   extends: ["stylelint-config-standard-scss", "stylelint-config-clean-order", "stylelint-config-recommended-vue/scss"],
-  plugins: ["stylelint-scss"]
+  plugins: ["stylelint-scss"],
+  rules: {
+    "selector-class-pattern": [
+      "^[a-z]+([a-z_]+|__[a-z]+)?$",
+      {
+        resolveNestedSelectors: true,
+      },
+    ],
+  },
 };
