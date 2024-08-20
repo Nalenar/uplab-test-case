2e433b2c60e95c6221c878dd91aeec6ed4b9076d
Nikita <nichrom.isme@gmail.com>
2024-08-20 07:51:10 +0300

    Added article route

--- src/router.ts
+++ src/router.ts
@@ -1,13 +1,16 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-import Home from "@/views/Home";
-import Test from "@/views/Test";
+import Home from "@views/Home";
+import Test from "@views/Test";
+import Articles from "@views/Articles";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: "/", component: Home },
-    { path: "/test", component: Test },
+    { path: "/", component: Home, name: "blog" },
+    { path: "/articles", component: Articles, name: "articles" },
+
+    { path: "/test", component: Test, name: "test" },
   ],
 });
 
