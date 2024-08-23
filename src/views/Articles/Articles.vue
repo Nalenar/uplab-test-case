9ff96a1963be17b3e632b927192d5737dacc9fa6
nikita.zinovyev <nikita.zinovyev.staff@gmail.com>
2024-08-23 05:15:33 +0300

    Created articles view

--- src/views/Articles/Articles.vue
+++ src/views/Articles/Articles.vue
@@ -1,3 +1,108 @@
 <template>
-  <div>Articles</div>
+  <div class="root">
+    <ArticlesHeader isWithFilters title="All Articles" />
+    <div class="margin" />
+    <ul class="articles" v-if="isArticlesExist && !loading">
+      <Article v-for="article in store.articles" :key="article.id" :article />
+    </ul>
+    <p v-else class="loading">Loading...</p>
+    <div class="btns">
+      <button v-if="!loading && store.articles.length !== 100" class="load" @click="handleLoadMore">
+        Load more...
+      </button>
+      <p v-else-if="loading" class="loading">Loading...</p>
+    </div>
+  </div>
 </template>
+
+<script setup lang="ts">
+import { ArticlesHeader } from "@views/Home";
+import { Article } from "./components";
+
+import { computed, onMounted, ref, watch } from "vue";
+
+import { useArticlesStore } from "@/stores";
+
+const store = useArticlesStore();
+
+// отвечает за подгрузку постов, передается в url запроса
+const counter = ref<number>(1);
+const loading = ref<boolean>(false);
+
+const isArticlesExist = computed(() => store.articles && store.articles.length > 0);
+
+const handleLoadMore = () => {
+  counter.value++;
+};
+
+onMounted(() => {
+  loading.value = true;
+  store.$reset();
+  store.fetchArticlesByPage(counter.value);
+  loading.value = false;
+});
+
+watch(counter, () => {
+  loading.value = true;
+  store.fetchArticlesByPage(counter.value);
+  loading.value = false;
+});
+</script>
+
+<style lang="scss" scoped>
+@import "@styles/functions";
+
+.root {
+  width: 100%;
+  height: 100%;
+  padding: to-rem(30) to-rem(50);
+}
+
+.margin {
+  margin-top: to-rem(30);
+}
+
+.articles {
+  display: flex;
+  flex-direction: column;
+  gap: to-rem(20);
+  width: 100%;
+}
+
+.loading {
+  width: 100%;
+  font-size: to-rem(24);
+  color: $color-logo;
+  text-align: center;
+}
+
+.btns {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+  padding: to-rem(30);
+
+  .load {
+    cursor: pointer;
+
+    padding: to-rem(10) to-rem(100);
+
+    font-weight: 500;
+    font-style: italic;
+    color: $color-text-main;
+
+    background: linear-gradient(270deg, $color-bg, rgb(151 186 238 / 83.6%));
+    border: 2px solid $color-text-secondary;
+    border-radius: to-rem(10);
+
+    transition: all 0.3s ease-in-out;
+
+    &:hover {
+      scale: 1.05;
+      border-color: $color-commented;
+    }
+  }
+}
+</style>
--- src/views/Articles/components/Article.vue
+++ src/views/Articles/components/Article.vue
@@ -1,3 +1,90 @@
 <template>
-  <div>Article</div>
+  <li class="article" @click="handleRedirect">
+    <p class="article__title">{{ props.article.title }}</p>
+    <p class="article__body">{{ props.article.body }}</p>
+    <div class="wrapper">
+      <p class="article__user">{{ user }}</p>
+      <p class="article__id">#{{ props.article.id }}</p>
+    </div>
+  </li>
 </template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+import { useRouter } from "vue-router";
+
+import { useUsersStore } from "@/stores";
+
+import type ArticleProps from "@/types/interfaces/Article";
+
+const props = defineProps<ArticleProps>();
+const router = useRouter();
+const store = useUsersStore();
+
+const user = computed(() => {
+  const user = store.getUser(props.article.userId);
+  if (user) return user?.name + "<" + user?.email + ">";
+  else return "error";
+});
+
+const handleRedirect = () => {
+  router.push(`/articles/${props.article.id}`);
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@styles/functions";
+
+.article {
+  cursor: pointer;
+
+  display: flex;
+  flex-direction: column;
+  gap: to-rem(10);
+  align-items: start;
+
+  padding: to-rem(15);
+
+  background: linear-gradient(270deg, $color-bg, rgb(151 186 238 / 83.6%));
+  border: 1px solid $color-text-secondary;
+  border-radius: 10px;
+  box-shadow: 10px 7px 12px -1px $color-text-secondary;
+
+  transition: all 0.3s ease-in-out;
+
+  &:hover {
+    transform: translate(5px, 5px);
+    border-color: $color-commented;
+    box-shadow: 10px 7px 12px 2px $color-text-secondary;
+  }
+
+  .wrapper {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+  }
+
+  &__id {
+    font-size: to-rem(14);
+    color: $color-text-secondary;
+    text-align: end;
+  }
+
+  &__title {
+    font-size: to-rem(24);
+    font-weight: 600;
+    color: $color-logo;
+  }
+
+  &__body {
+    font-size: to-rem(16);
+  }
+
+  &__user {
+    font-weight: 300;
+    font-style: italic;
+    color: rgb(94 94 196);
+  }
+}
+</style>
