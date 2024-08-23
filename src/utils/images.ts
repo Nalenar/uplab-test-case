91a55cc6dc5249e8bad8654e12f9120ebd5cd28b
nikita.zinovyev <nikita.zinovyev.staff@gmail.com>
2024-08-23 08:19:59 +0300

    fix build error

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -1,5 +1,5 @@
-import heroImg1 from "/images/main1.jpg";
-import heroImg2 from "/images/main2.jpg";
-import heroImg3 from "/images/main3.jpg";
+import heroImg1 from "@images/main1.jpg";
+import heroImg2 from "@images/main2.jpg";
+import heroImg3 from "@images/main3.jpg";
 
 export const heroImgArr = [heroImg1, heroImg2, heroImg3];
